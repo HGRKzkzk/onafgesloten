8d3affa4c2d0e7c9ab536c4129a267dd98e9236d
HGRKzkzk <31103706+HGRKzkzk@users.noreply.github.com>
2026-09-02 19:25:33 +0200

    Voeg academische begrippen toe aan glossary

--- src/data/glossary.ts
+++ src/data/glossary.ts
@@ -68,6 +68,69 @@ export const glossary: Record<string, GlossaryEntry> = {
       'Een reactie waarin wat iemand of iets raakt niet alleen wordt ondergaan, maar op een eigen manier wordt opgenomen en beantwoord.'
   },
 
+
+  processfilosofie: {
+    term: 'Procesfilosofie',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'Een filosofische familie waarin worden, verandering en proces fundamenteel zijn voor wat werkelijkheid is, in plaats van alleen eigenschappen van blijvende dingen.',
+    url: 'https://en.wikipedia.org/wiki/Process_philosophy'
+  },
+
+  'relationele-autonomie': {
+    term: 'Relationele autonomie',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'Een benadering van autonomie waarin zelfstandigheid niet losstaat van sociale relaties, afhankelijkheden en de voorwaarden waaronder iemand handelingsvermogen ontwikkelt.',
+    url: 'https://plato.stanford.edu/entries/feminism-autonomy/'
+  },
+
+  'belichaamde-cognitie': {
+    term: 'Belichaamde cognitie',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'Een benadering waarin cognitie niet uitsluitend in het brein wordt geplaatst, maar mede wordt gevormd door lichaam, handelen en interactie met de omgeving.',
+    url: 'https://plato.stanford.edu/entries/embodied-cognition/'
+  },
+
+  homeostase: {
+    term: 'Homeostase',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'Het reguleren van interne fysiologische toestanden rond functionele grenzen of evenwichten, ondanks veranderingen in organisme en omgeving.',
+    url: 'https://en.wikipedia.org/wiki/Homeostasis'
+  },
+
+  allostase: {
+    term: 'Allostase',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'Een regulatieconcept waarin stabiliteit wordt bereikt door dynamische aanpassing aan verwachte of veranderende omstandigheden.',
+    url: 'https://en.wikipedia.org/wiki/Allostasis'
+  },
+
+  substantie: {
+    term: 'Substantie',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'In de metafysica: datgene wat als een min of meer zelfstandig bestaand ding wordt opgevat en eigenschappen kan dragen of veranderingen kan ondergaan.',
+    url: 'https://plato.stanford.edu/entries/substance/'
+  },
+
+  'persoonlijke-identiteit': {
+    term: 'Persoonlijke identiteit',
+    type: 'gangbaar',
+    chapterOfOrigin: '01-wat-verschijnt-verschijnt-onaf',
+    short:
+      'Het filosofische probleem wat iemand door de tijd heen tot dezelfde persoon maakt, ondanks lichamelijke, psychologische en relationele verandering.',
+    url: 'https://plato.stanford.edu/entries/identity-personal/'
+  }
   draagkracht: {
     term: 'Draagkracht',
     type: 'precisering',
