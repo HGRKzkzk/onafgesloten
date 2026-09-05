395fbb546d0fbe0e603d24535e99add8365738e3
HGRKzkzk <31103706+HGRKzkzk@users.noreply.github.com>
2026-09-05 16:29:01 +0200

    Redirect oude hoofdstuk-14-route

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ const site = process.env.DEPLOY_SITE || 'https://woutervanitterzon.nl';
 export default defineConfig({
   site,
   base,
-  trailingSlash: 'always'
+  trailingSlash: 'always',
+  redirects: {
+    '/hoofdstukken/14-twee-toetsgevallen-wat-verandert-er-wanneer-oov-iets-moet-doen/':
+      '/hoofdstukken/14-drie-toetsgevallen-wat-verandert-er-wanneer-dit-denken-iets-moet-doen/'
+  }
 });
